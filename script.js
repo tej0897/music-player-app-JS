f1d927e530fe8a4b7e8d4ffb5a9718ab8e33e1fd
Tejas Kumar S <tej0897@gmail.com>
2024-04-28 02:28:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -131,8 +131,11 @@ const playPreviousSong = () => {
 
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
   
-  
+
 };
 
 const renderSongs = (array) => {
