3e5ea1470fe512487e425a5ea05e5eb796a6a168
Tejas Kumar S <tej0897@gmail.com>
2024-04-28 13:00:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -134,6 +134,10 @@ const highlightCurrentSong = () => {
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
   
 
 };
